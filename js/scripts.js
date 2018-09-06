e4b951bbc0f9e7c97406193043e0b11cb86dc4ee
LaszloKreizler <42902293+LaszloKreizler@users.noreply.github.com>
2018-09-06 12:48:02 -0300

    Cambios Formulario

--- js/scripts.js
+++ js/scripts.js
@@ -30,22 +30,24 @@ $(document).ready(function(){
         return false;
     });
 
+    //FancyBox Galería
+
+    $(".img-galeria").fancybox({
+        openEffect : 'elastic',   //'fade', 'elastic'
+        closeEffect	: 'elastic',
+        openSpeed:'normal', //ms, slow, normal, fast (default 250ms)
+        closeSpeed:'normal',
+        helpers : {
+        title : {
+            type : 'inside' //'float', 'inside', 'outside' or 'over'
+        },
+        overlay : {
+            closeClick : true  // if true, se cierra al hacer click fuera de la imagen
+        }
+    },
+    padding:11
+    });
+
   });
 
-  //FancyBox Galería
-
-  $(".img-galeria").fancybox({
-    openEffect : 'elastic',   //'fade', 'elastic'
-    closeEffect	: 'elastic',
-    openSpeed:'normal', //ms, slow, normal, fast (default 250ms)
-    closeSpeed:'normal',
-    helpers : {
-      title : {
-         type : 'inside' //'float', 'inside', 'outside' or 'over'
-      },
-      overlay : {
-        closeClick : true  // if true, se cierra al hacer click fuera de la imagen
-      }
-  },
-  padding:11
-});
+  
